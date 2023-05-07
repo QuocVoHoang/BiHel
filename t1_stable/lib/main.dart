6578d1f52f9a81ebb49175711e8e87438f08e690
quoc.vo-hoang <vohoangquoc992001@gmail.com>
2023-05-07 08:59:19 +0700

    update 7/5

--- t1_stable/lib/main.dart
+++ t1_stable/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const TopLogin()
+      home: const TopLogin(),
     );
   }
 }
